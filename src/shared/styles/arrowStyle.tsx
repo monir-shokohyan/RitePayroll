e9a2b86e175f3bb73a202cc438dc0c4c507e47b6
m monir shokohyan <monir.shekoyans1@gmail.com>
2025-12-15 13:35:42 +0300

    feat: add the social media icons button

--- src/shared/styles/arrowStyle.tsx
+++ src/shared/styles/arrowStyle.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 export const HoveredArrow = styled.div`
   transition: transform 0.2s ease-in-out;
+  z-index: 9999;
 
   &:hover {
     transform: translateY(10px);
