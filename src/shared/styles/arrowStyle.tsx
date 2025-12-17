fa46419fc954c3723acc2e183d8d749f2f53c9e8
m monir shokohyan <monir.shekoyans1@gmail.com>
2025-12-17 10:56:43 +0300

    fix: fix the issue of radius

--- src/shared/styles/arrowStyle.tsx
+++ src/shared/styles/arrowStyle.tsx
@@ -2,9 +2,8 @@ import styled from 'styled-components'
 
 export const HoveredArrow = styled.div`
   transition: transform 0.2s ease-in-out;
-  z-index: 9999;
 
   &:hover {
-    transform: translateY(10px);
+    transform: translateY(10px) !important;
   }
 `
