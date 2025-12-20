27ad518f9894f98fc735c38a31d8a06591e673e6
m monir shokohyan <monir.shekoyans1@gmail.com>
2025-12-20 13:25:16 +0300

    fix: fix the issue of social media button

--- src/features/social button/styles/styles.ts
+++ src/features/social button/styles/styles.ts
@@ -55,7 +55,13 @@ export const MenuItem = styled(motion.a)<MotionaType>`
   display: flex;
   align-items: center;
   justify-content: center;
-  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);
+  box-shadow:
+    0 15px 25px -4px rgba(0, 0, 0, 0.2),
+    inset 0 -3px 4px -1px rgba(0, 0, 0, 0.1),
+    0 -10px 15px -1px rgba(255, 255, 255, 0.1),
+    inset 0 3px 4px -1px rgba(255, 255, 255, 0.1),
+    inset 0 0 5px 1px rgba(255, 255, 255, 0.1),
+    inset 0 20px 30px 0 rgba(255, 255, 255, 0.1);
   transition: transform 0.5s ease-out;
   &:hover {
     transform: scale(1.1);
