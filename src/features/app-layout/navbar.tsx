7c2f60d6d9ded21dd990587761295de54a0cb339
m monir shokohyan <monir.shekoyans1@gmail.com>
2026-01-06 16:27:27 +0300

    feat: add new welcome feature info

--- src/features/app-layout/navbar.tsx
+++ src/features/app-layout/navbar.tsx
@@ -86,7 +86,6 @@ const Navbar = memo(() => {
           Contact Us
         </MenuItems>
         <Menu
-          width={200}
           position="bottom"
           withArrow
           shadow="md"
