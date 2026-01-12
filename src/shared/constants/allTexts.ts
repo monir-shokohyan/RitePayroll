26ac0e399967490aabc058b1c3f4eeb058e7f3a0
m monir shokohyan <monir.shekoyans1@gmail.com>
2026-01-12 12:46:10 +0300

    fix: fix the contact page title

--- src/shared/constants/allTexts.ts
+++ src/shared/constants/allTexts.ts
@@ -485,8 +485,8 @@ export const TotalData: TotalDataItem[] = [
   },
   {
     name: 'contact us',
-    dTitle: 'What features does RitePayroll have?',
-    title: 'Here, check out our features',
+    dTitle: 'Get in Touch',
+    title: 'We’re here to assist you — choose your preferred way to connect',
     description:
       "We'd love to hear from you and start something special together. Call us with any questions.",
     target: '/',
